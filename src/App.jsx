0d1a96a7c5267751075c6d9779f3f8bc2189d1b3
KevinFrutos <vinke.14297@gmail.com>
2022-01-01 02:43:27 +0100

    Linear Gradient

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 
 const App = () => {
 	const txt_color = useRef(null);
+	const [color, setColor] = useState("#000000");
 	const hexadecimal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 	let colorHex = "#";
-	const [color, setColor] = useState("#000000");
 
 	const changeColor = () => {
 		for (let i = 0; i < 6; i++) {
@@ -14,7 +14,7 @@ const App = () => {
 		}
 		setColor(colorHex);
 		txt_color.current.style.color = colorHex;
-		document.body.style.backgroundColor = colorHex;
+		document.body.style.backgroundImage = `linear-gradient(#000, ${colorHex})`;
 	};
 
 	const copyClipboard = () => {
